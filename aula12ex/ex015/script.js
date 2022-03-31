35010286c1ea23758f8f64c9186900c9aa1973de
Sandra Gaspar <93771678+SandraDiasGaspar@users.noreply.github.com>
2022-03-31 16:21:04 +0100

    alterações

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -12,36 +12,28 @@ function verificar() {
         if (fsex[0].checked) {
             género = "homem"
             if (idade >=0 && idade < 10) {
-                // criança
                 img.setAttribute("src", "foto-bebe-m.png")
             } else if (idade < 21) { 
-                // jovem
                 img.setAttribute("src", "foto-jovem-m.png")
-            } else if (idade < 40) {
-                // adulto
+            } else if (idade < 50) {
                 img.setAttribute("src", "foto-adulto-m.png")
-            } else (idade < 60) {
-                // idoso
+            } else {
                 img.setAttribute("src", "foto-idoso-m.png")
             }
         } else if (fsex[1].checked) {
             género = "mulher"
             if (idade >=0 && idade < 10) {
-                // criança
                 img.setAttribute("src", "foto-bebe-f.png")
             } else if (idade < 21) { 
-                // jovem
                 img.setAttribute("src", "foto-jovem-f.png")
-            } else if (idade < 40) {
-                // adulto
+            } else if (idade < 50) {
                 img.setAttribute("src", "foto-adulto-f.png")
-            } else (idade < 60) {
-                // idoso
+            } else {
                 img.setAttribute("src", "foto-idoso-f.png")
             }
         }
-    }
         res.style.textAlign = "center"
         res.innerHTML = `Detectamos ${género} com ${idade} anos.`
         res.appendChild(img)
     }
+}
